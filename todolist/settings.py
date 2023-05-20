76af38f029ddff5f228a4c08e6d65aad45a0f610
yury-yury <yury_yury@mail.ru>
2023-05-20 22:28:33 +0300

    init new repository

--- todolist/settings.py
+++ todolist/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
 ]
 
@@ -90,6 +91,7 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
